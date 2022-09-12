da5658ccc30e142600109a766f9a2f297e2c9e2d
CoderB <coderbridges@gmail.com>
2022-09-12 12:50:26 -0700

    add prompt for rock, paper, or scissors and return response if rock is chosen

--- rock.js
+++ rock.js
@@ -11,3 +11,35 @@ function getComputerChoice() {
     let index = Math.floor(Math.random() * arr.length);
     return arr[index];
 }
+
+function playRound(playerSelection, computerSelection) {
+     playerSelection = prompt("rock, paper, scissors?");
+     computerSelection = getComputerChoice();
+
+     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
+         return 'You Tie!';
+     }
+     if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+        return 'You Lose! Paper beats Rock!';
+    }
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
+        return 'You Win! Rock beats Scissors!';
+    }
+           
+        //THE METHOD BELOW USES switch()
+
+   // if (playerSelection.toLowerCase() === 'rock') {
+   //     switch (computerSelection) {
+   //         case computerSelection = 'rock':
+   //             console.log(" You Tie!");
+   //             break;
+   //         case computerSelection = 'paper':
+   //            console.log("You Lose! Paper beats Rock");
+   //            break;
+   //         case computerSelection = 'scissors':
+   //             console.log("You Win! Rock beats scissors");
+   //             break;
+    //    }
+
+    }
+
