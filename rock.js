19609d58d5a4c6fa3856d583db7714eba91c9793
CoderB <coderbridges@gmail.com>
2022-09-12 15:02:19 -0700

    add game() function to play 5 times, add player win count and computer win count, add console.log to display the win or loss out of 5 and reset player and computer win count

--- rock.js
+++ rock.js
@@ -1,49 +1,51 @@
-console.log("hello world!");
-
-
-//console.log(arr[0]);
-
-//console.log(arr.length);
-
 let arr = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     let index = Math.floor(Math.random() * arr.length);
     return arr[index];
 }
+let playerWin = 0;
+let computerWin = 0;
 
 function playRound(playerSelection, computerSelection) {
      playerSelection = prompt("rock, paper, scissors?");
      computerSelection = getComputerChoice();
+
 
      if (playerSelection.toLowerCase() === 'rock') {
          if (computerSelection === 'rock'){
              return 'You Tie!';
          }
          else if (computerSelection === 'paper'){
+             computerWin++;
              return 'You Lose! Paper beats Rock!';
          }
          else if (computerSelection === 'scissors'){
+             playerWin++;
              return 'You Win! Rock beats Scissors!';
          }
      }
      else if (playerSelection.toLowerCase() === 'paper') {
         if (computerSelection === 'rock'){
+            playerWin++;
             return 'You Win! Paper beats Rock!';
         }
         else if (computerSelection === 'paper'){
             return 'You Tie!';
         }
         else if (computerSelection === 'scissors'){
+            computerWin++;
             return 'You Lose! Scissors beats Paper!';
         }
 
      }
      else if (playerSelection.toLowerCase() === 'scissors') {
         if (computerSelection === 'rock'){
+            computerWin++;
             return 'You Lose! Rock beats Scissors!';
         }
         else if (computerSelection === 'paper'){
+            playerWin++;
             return 'You Win! Scissors beats Paper';
         }
         else if (computerSelection === 'scissors'){
@@ -54,6 +56,31 @@ function playRound(playerSelection, computerSelection) {
          return 'Please choose rock, paper, or scissors';
      }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++){
+        console.log(playRound());     
+    }
+    if (playerWin > computerWin) {
+        console.log(`You Won! ${playerWin} - ${computerWin}`);
+        playerWin = 0;
+        coumputerWin = 0;
+    }
+    else if (playerWin < computerWin) {
+        console.log(`You Lost! ${playerWin} - ${computerWin}`);
+        playerWin = 0;
+        coumputerWin = 0;
+    }
+    else if (playerWin === computerWin) {
+        console.log(`You Tied! ${playerWin} - ${computerWin}`);
+        playerWin = 0;
+        coumputerWin = 0;
+    }
+}
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
 
         // THE METHOD BELOW IS ONE OF MY SOLUTIONS
    //  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock'){
