9c9f373014bf28c2e7b78b1a557e9767f41c1c21
CoderB <coderbridges@gmail.com>
2022-09-12 11:26:25 -0700

    add getComputerChoice function that returns either rock,paper, or scissors

--- rock.js
+++ rock.js
@@ -1,1 +1,13 @@
 console.log("hello world!");
+
+
+//console.log(arr[0]);
+
+//console.log(arr.length);
+
+let arr = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
